b8470f69382ee387fea1a90b156b9d5de840ab9a
jihyun <noblea1117@gmail.com>
2015-11-15 18:08:05 +0900

    add game turn

--- player.h
+++ player.h
@@ -3,11 +3,20 @@
 #define NOT_PLAY    0 // play 한 적 없음
 #define PLAYED      1 // play 한 적 있음
 
+
+typedef enum _game_turn {
+    // 게임 차례 체크
+    GAME_TURN_NOT_NOW,
+    GAME_TURN_NOW
+
+} game_turn;
+
 typedef struct _player {
     char id[5]; // player ID
     int score; // player 점수
     int played;// 이전에 play했는지 체크
     int money; // player가 갖고 있는 돈
+    int turn;
     // TODO: 갖고 있는 패, card에 대한 정보
 
 } player;
