15ee0759ca5867434ddc36753f56833bdaba73ca
jihyun <noblea1117@gmail.com>
2015-11-26 23:49:58 +0900

    fix indent

--- status.c
+++ status.c
@@ -3,7 +3,6 @@
 #include <ctype.h>
 #include <stdio.h>
 // player 구조체에 점수 세팅
-int gusip=0;
 void setScore(player *playerId) {
 	player* tmp = NULL;
 
@@ -20,93 +19,111 @@ void setScore(player *playerId) {
 
 	head = playerId;
 
-	for(i=0 ; head->eating_card->next != NULL ; i++){
-		if((((head->eating_card->next->data)%4 == 2)||((head->eating_card->next->data)%4 == 3))
-			&& (((head->eating_card->next->data) != 46)||((head->eating_card->next->data) != 47))){
-			pi++;}
-		if(((head->eating_card->next->data) == 41)||((head->eating_card->next->data) == 47)){
-			ssangpi++;}
+	for(i=0 ; head->eating_card->next != NULL ; i++) {
+	    if((((head->eating_card->next->data)%4 == 2) || ((head->eating_card->next->data)%4 == 3)) && (((head->eating_card->next->data) != 46) || ((head->eating_card->next->data) != 47))) {
+			pi++;
+        }
+        
+		if(((head->eating_card->next->data) == 41) || ((head->eating_card->next->data) == 47)) {
+		    ssangpi++;
+        }
 
+		if(((head->eating_card->next->data) == 0) || ((head->eating_card->next->data) == 8) || ((head->eating_card->next->data)==28) || ((head->eating_card->next->data)==40)) {
+			gwang++;
+        }
 
-		if(((head->eating_card->next->data) == 0)||((head->eating_card->next->data)==8)
-		     || ((head->eating_card->next->data)==28)||((head->eating_card->next->data)==40)){
-			gwang++;}
 		if((head->eating_card->next->data) == 44){
 			gwang++, bigwang++;}
 
 
-		if((((head->eating_card->next->data)%4 == 1)
-			&& (((head->eating_card->next->data) != 29)||((head->eating_card->next->data)!=41)
-			|| ((head->eating_card->next->data) != 45)))
-			||(head->eating_card->next->data == 46)){
-			oh++;}
-		if(((head->eating_card->next->data) == 1)||((head->eating_card->next->data)==5)||
-				((head->eating_card->next->data)==9)){
-			hongdan++;}
-		if(((head->eating_card->next->data) == 13)||((head->eating_card->next->data)==17)||
-				((head->eating_card->next->data)==23)){
-			chodan++;}
-		if(((head->eating_card->next->data) == 21)||((head->eating_card->next->data)==33)||
-					((head->eating_card->next->data)==37)){
-			chungdan++;}
+		if((((head->eating_card->next->data)%4 == 1) && (((head->eating_card->next->data) != 29) || ((head->eating_card->next->data)!=41) || ((head->eating_card->next->data) != 45))) ||(head->eating_card->next->data == 46)) {
+		    oh++;
+        }
 
+		if(((head->eating_card->next->data) == 1) || ((head->eating_card->next->data) == 5) || ((head->eating_card->next->data)==9)) {
+			hongdan++;
+        }
 
-		if(((head->eating_card->next->data) == 4)||
-		((head->eating_card->next->data)==12)||
-		((head->eating_card->next->data)==16)||
-		 ((head->eating_card->next->data)==20)||
-		  ((head->eating_card->next->data)==24)||
-			((head->eating_card->next->data)==29)||
-			((head->eating_card->next->data)==36)||
-			((head->eating_card->next->data)==45)||(((head->eating_card->next->data)==32)&&gusip==1)){
-			sip++;} // 구십(32)가 십으로 쓰일 때
-		if(((head->eating_card->next->data) == 4)||
-				((head->eating_card->next->data)==12)||
-				((head->eating_card->next->data)==33)){
-			godori++;}
+		if(((head->eating_card->next->data) == 13) || ((head->eating_card->next->data) == 17) || ((head->eating_card->next->data) == 23)) {
+			chodan++;
+        }
 
+		if(((head->eating_card->next->data) == 21) || ((head->eating_card->next->data) == 33) || ((head->eating_card->next->data)==37)) {
+			chungdan++;
+        }
 
-		if((head->eating_card->next->data) == 32){
-			pi=pi+2;}   // 구십(32)가 피로 쓰였을때
-	head->eating_card->next = head->eating_card->next->next;}
+        // 구십(32)가 십으로 쓰일 때
+		if(((head->eating_card->next->data) == 4) || ((head->eating_card->next->data) == 12) || ((head->eating_card->next->data) == 16) || ((head->eating_card->next->data) == 20) || ((head->eating_card->next->data) == 24) || ((head->eating_card->next->data) == 29) || ((head->eating_card->next->data) == 36) || ((head->eating_card->next->data) == 45) || (((head->eating_card->next->data) == 32) && gusip == 1)) {
+			sip++;
+        } 
 
-	if(ssangpi == 1){
-		pi = pi+2;}
-	if(ssangpi == 2){
-		pi = pi+4;}
-	//if(gukjin == 1){
-	//	pi = pi+2;} // 국진을 대비해서. (통합 과정에서 필요없어짐.)
-	if( pi >= 10){
-		tmp->score = tmp->score + pi-9;}
+		if(((head->eating_card->next->data) == 4) || ((head->eating_card->next->data) == 12)|| ((head->eating_card->next->data) == 33)) {
+			godori++;
+        }
 
+        // 구십(32)가 피로 쓰였을 때
+		if((head->eating_card->next->data) == 32) {
+			pi=pi+2;
+        } 
 
-	if(gwang == 3){
+	    head->eating_card->next = head->eating_card->next->next;
+    }
+
+	if(ssangpi == 1) {
+		pi = pi+2;
+    }
+    
+	if(ssangpi == 2) {
+		pi = pi+4;
+    }
+
+	if(pi >= 10) {
+		tmp->score = tmp->score + pi-9;
+    }
+
+	if(gwang == 3) {
 		tmp->score = tmp->score + 3;
 	}
-	if((gwang == 3) &&(bigwang ==1 ))
-	{
-		tmp->score = tmp->score -1;}
-	if(gwang == 4){
-		tmp->score = tmp->score +1 ;}
-	if((gwang == 4) &&(bigwang ==1))
-	{
-		tmp->score = tmp->score +1;}
-	if(gwang ==5){
-		tmp->score = tmp->score +11;}
 
-	if(oh >= 5){
-		tmp->score = tmp->score + oh-4;}
-	if(hongdan ==3){
-		tmp->score = tmp->score + 3;}
-	if(chodan==3){
-		tmp->score = tmp->score + 3;}
-	if(chungdan==3){
-		tmp->score = tmp->score + 3;}
+	if((gwang == 3) && (bigwang ==1)) {
+	    tmp->score = tmp->score - 1;
+    }
 
-	if(sip >= 5){
-		tmp->score = tmp->score + sip-4;}
-	if(godori==3){
-		tmp->score = tmp->score + 5;}
+	if(gwang == 4) {
+		tmp->score = tmp->score + 1;
+    }
+
+	if((gwang == 4) && (bigwang == 1)) {
+		tmp->score = tmp->score + 1;
+    }
+
+	if(gwang == 5) {
+		tmp->score = tmp->score + 11;
+    }
+
+	if(oh >= 5) {
+		tmp->score = tmp->score + oh-4;
+    }
+
+	if(hongdan == 3) {
+		tmp->score = tmp->score + 3;
+    }
+
+	if(chodan == 3) {
+		tmp->score = tmp->score + 3;
+    }
+
+	if(chungdan == 3) {
+		tmp->score = tmp->score + 3;
+    }
+
+	if(sip >= 5) {
+		tmp->score = tmp->score + sip-4;
+    }
+
+	if(godori == 3) {
+		tmp->score = tmp->score + 5;
+    }
 
 	playerId->score = tmp->score;
 }
@@ -138,28 +155,29 @@ void setMoney(player *playerId) {
 	head = (player*)malloc(sizeof(player));
 	head = playerId;
 
-	for(i=0 ; head->eating_card->next != NULL ; i++){
-		if((((head->eating_card->next->data)%4 == 2)||((head->eating_card->next->data)%4 == 3))
-			&& (((head->eating_card->next->data) != 46)||((head->eating_card->next->data) != 47))){
-			pi++;}
-		if(((head->eating_card->next->data) == 41)||((head->eating_card->next->data) == 47)){
-			pi =pi+2;}
+	for(i=0 ; head->eating_card->next != NULL ; i++) {
+		if((((head->eating_card->next->data)%4 == 2) || ((head->eating_card->next->data)%4 == 3)) && (((head->eating_card->next->data) != 46)||((head->eating_card->next->data) != 47))) {
+			pi++;
+        }
+
+		if(((head->eating_card->next->data) == 41) || ((head->eating_card->next->data) == 47)) {
+			pi =pi+2;
+        }
+
 		head->eating_card->next = head->eating_card->next->next;}
 
-// 피박(해결)  고박(진사람들 쪽을 조사해야함)- (4,5번할때) 
-// 쓰리고, 포고, 흔들기, 멍텅구리(이와같은 것을 하고 이 긴사람 점수를 두배해주는 식으로 해결하면 됨)- (4,5번 할때)
+    //  TODO: 피박(해결)  고박(진사람들 쪽을 조사해야함)- (4,5번할때) 
+    // 쓰리고, 포고, 흔들기, 멍텅구리(이와같은 것을 하고 이 긴사람 점수를 두배해주는 식으로 해결하면 됨)- (4,5번 할때)
 											   
-	if(playerId->id == tmp->id)
-	{
+	if(playerId->id == tmp->id) {
 		playerId->money = INITIAL_MONEY + (tmp->score)*100*2;
-	}
-	else{
-	if(pi<7){
-		playerId->money = INITIAL_MONEY - (tmp->score)*100*2;       //피박문제 해결한줄. 
-		tmp->money = (tmp->money) + (tmp->score)*100;}				//피박문제 해결한줄.
-	else
-		playerId->money = INITIAL_MONEY - (tmp->score)*100;
-	}
+	} else {
+        if(pi < 7) {
+            playerId->money = INITIAL_MONEY - (tmp->score)*100*2;       //피박문제 해결한줄. 
+            tmp->money = (tmp->money) + (tmp->score)*100;}				//피박문제 해결한줄.
+        else
+            playerId->money = INITIAL_MONEY - (tmp->score)*100;
+    }
 }
 
 
@@ -282,11 +300,15 @@ void drawInterface(char *command) {
 	card *eating=NULL;
 	card *holding=NULL;
 	card target;
-	if(isdigit(command[0]))
-	{if(command[0] == '9') num = 8;
-	 else 
-		num = 6;
+	if(isdigit(command[0])) {
+        if(command[0] == '9') {
+            num = 8;
+        }
+	    else {
+		    num = 6;
+        }
 	}
+
 	else if(command[0] == 'g') num=1;			
 	else if(command[0] == 's' && command[1] == 'a') num=9;
 	else if(command[0] == 's' && command[1] != 'a') num=2;			
@@ -299,26 +321,27 @@ void drawInterface(char *command) {
 				
     switch(num) {
         case 1:
-		    system("clear");				
-
             printf("go 함수\n");
             // blah blah... 
 
             break;
+
 		case 2: 
-		    if(players[getTurn()].turn == GAME_TURN_NOW && players[getTurn()].score>=3)
-			{printf("STOP 합니다, 게임을 종료합니다.\n");
-			 exit(0);
+		    if(players[getTurn()].turn == GAME_TURN_NOW && players[getTurn()].score>=3) {
+                printf("STOP 합니다, 게임을 종료합니다.\n");
+			    exit(0);
 			}		
 
             printf("stop 함수\n");
             
 			break;
+
 		case 3: 
             printf("게임을 종료합니다\n");
 			exit(0);
 
 			break;
+
 		case 4: 
             printf("-------게이머의 잔고---------\n");
             printf("A의 잔고 : %d 원\n", getMoney(players));
@@ -326,6 +349,7 @@ void drawInterface(char *command) {
             printf("C의 잔고 : %d 원\n", getMoney(players+2));
 
 			break;
+
 		case 5: 
             printf("--------- 도움말 ------------\n");
             printf("1. g(o) : 고 (자기 turn에 점수가 났고 3점이상");
@@ -340,6 +364,7 @@ void drawInterface(char *command) {
             printf("10. load : 파일에 저장된 상태를 읽어서 계속 게임 진행\n");
 
 			break;
+
 		case 6:
             index = command[0]-'0';
 			turn = getTurn();
@@ -362,115 +387,135 @@ void drawInterface(char *command) {
 					}
 					else if(same==2)
 					{
-					cardInsert(eating,getSecondCard(blanketCard,target.data)->data);
-					cardDelete(holding,target.data);
-          			cardDelete(dummyCard,getSecondCard(blanketCard,target.data)->data);
+                        cardInsert(eating,getSecondCard(blanketCard,target.data)->data);
+                        cardDelete(holding,target.data);
+                        cardDelete(dummyCard,getSecondCard(blanketCard,target.data)->data);
 					}
 					else
-					printf("잘못된 선택입니다");
-				 	}
+					    printf("잘못된 선택입니다");
+				}
+
                 cardInsert(eating,target.data);
                 cardDelete(holding,target.data);
                 cardDelete(blanketCard,target.data);
-        	    }
-				break;
-	case 8: printf("9십을 십으로 이동합니다(default : 십->피)\n");
-			if(getCard(eating,32)!=NULL && gusip==0)
-			gusip=1;
-			break;
-	case 9: save();
-			break;	
-		
+        	}
 
+			break;
+            
+        case 8: 
+            printf("9십을 십으로 이동합니다(default : 십->피)\n");
+            if(getCard(eating,32)!=NULL && gusip==0) {
+                gusip=1;
+            }
+
+            break;
+
+        case 9: 
+            save();
+
+            break;	
     }
 }
 
-void save()
-{	
+void save() {	
 	card *blank = blanketCard;
 	FILE *ofp1;
+
 	ofp1 = fopen("blank.txt","w");
-	while(blank!=NULL)
-{
-	fprintf(ofp1,"%d ",blank->data);
-	blank = blank -> next;
-}
-fclose(ofp1);
+
+	while(blank!=NULL) {
+        fprintf(ofp1,"%d ",blank->data);
+        blank = blank -> next;
+    }
+
+    fclose(ofp1);
 
 	FILE *ofp20; // 플레이어0 의 정보
+
 	ofp20 = fopen("player0.txt","w");
 	fprintf(ofp20,"%d %d %d %d %d %p %p",players[0].id, players[0].score,players[0].played,players[0].money,players[0].turn);	
+
 	fclose(ofp20);
+
 	FILE *ofp21,*ofp22;
 	card *aeat,*ahold;
+
 	ofp21 = fopen("player0eat.txt","w");
 	aeat = players[0].eating_card;
-	while(aeat != NULL)
-	{
-	fprintf(ofp21,"%d ",aeat->data);
-	aeat = aeat -> next;
+
+	while(aeat != NULL) {
+        fprintf(ofp21,"%d ",aeat->data);
+        aeat = aeat -> next;
 	}
+
 	fclose(ofp21);
+
 	ofp22 = fopen("player0hold.txt","w");
 	ahold = players[0].holding_card;
-	while(ahold != NULL)
-	{
-	fprintf(ofp21,"%d ",ahold->data);
-	ahold = ahold -> next;
+
+	while(ahold != NULL) {
+        fprintf(ofp21,"%d ",ahold->data);
+        ahold = ahold -> next;
 	}
+
 	fclose(ofp22);
 
 	FILE *ofp30; // 플레이어1 의 정보
+
 	ofp30 = fopen("player1.txt","w");
 	fprintf(ofp30,"%d %d %d %d %d %p %p",players[1].id, players[1].score,players[1].played,players[1].money,players[1].turn);	
+
 	fclose(ofp30);
 
 	FILE *ofp31,*ofp32;
 	card *beat,*bhold;
+
 	ofp31 = fopen("player1eat.txt","w");
 	beat = players[1].eating_card;
-	while(beat != NULL)
-	{
-	fprintf(ofp31,"%d ",beat->data);
-	beat = beat -> next;
+
+	while(beat != NULL) {
+        fprintf(ofp31,"%d ",beat->data);
+        beat = beat -> next;
 	}
+
 	fclose(ofp31);
 
 	ofp32 = fopen("player1hold.txt","w");
 	bhold = players[1].holding_card;
-	while(bhold != NULL)
-	{
-	fprintf(ofp31,"%d ",bhold->data);
-	bhold = bhold -> next;
+
+	while(bhold != NULL) {
+        fprintf(ofp31,"%d ",bhold->data);
+        bhold = bhold -> next;
 	}
+
 	fclose(ofp32);
 
 	FILE *ofp40; // 플레이어2 의 정보
+
 	ofp40 = fopen("player2.txt","w");
 	fprintf(ofp40,"%d %d %d %d %d %p %p",players[2].id, players[2].score,players[2].played,players[2].money,players[2].turn);	
 	fclose(ofp40);
 
 	FILE *ofp41,*ofp42;
 	card *ceat,*chold;
+
 	ofp41 = fopen("player2eat.txt","w");
 	ceat = players[2].eating_card;
-	while(ceat != NULL)
-	{
-	fprintf(ofp41,"%d ",ceat->data);
-	ceat = ceat -> next;
+
+	while(ceat != NULL) {
+        fprintf(ofp41,"%d ",ceat->data);
+        ceat = ceat -> next;
 	}
+
 	fclose(ofp41);
 
 	ofp42 = fopen("player2hold.txt","w");
 	chold = players[2].holding_card;
-	while(chold != NULL)
-	{
-	fprintf(ofp41,"%d ",chold->data);
-	chold = chold -> next;
+
+	while(chold != NULL) {
+        fprintf(ofp41,"%d ",chold->data);
+        chold = chold -> next;
 	}
+
 	fclose(ofp42);
 }
-	
-
-
-
--- status.h
+++ status.h
@@ -23,6 +23,8 @@ typedef struct _game_status {
 
 } game_status;
 
+int gusip;
+
 void setScore(player *playerId);
 int getScore(player *playerId);
 void setMoney(player *playerId);
@@ -31,9 +33,9 @@ player* setWinner();
 void setPlayerInfo();
 void setTurn();
 int getTurn();
-void save();
 void drawScreen(); // status 변할 때마다 화면에 값 그려주기
                    // 플레이어 차례, 점수, 갖고 있는 패 등 그려주기
 void drawInterface(char *command); // 사용자 인터페이스 처리
+void save();
 
 #endif
