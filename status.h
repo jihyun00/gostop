39fa6ad7032aefa215ad027a48a59173de7f46f1
ssu_user <ssu_user@ssu208.(none)>
2015-12-01 01:42:58 +0900

    광박, 피박구현

--- status.h
+++ status.h
@@ -25,12 +25,12 @@ typedef struct _game_player_card_status {
 	int gwang;
 } P_C_S;
 
-//P_C_S[MAX_NUMBER_OF_PLAYER];
+P_C_S PlayerStatus[MAX_NUMBER_OF_PLAYER];
 
 
 void setScore(int playerId);
 int getScore(int playerId);
-void setMoney(int playerId, P_C_S );
+void setMoney(int playerId, P_C_S PlayerStatus[]);
 int getMoney(int playerId);
 player* setWinner();
 void drawScreen(); // status 변할 때마다 화면에 값 그려주기
